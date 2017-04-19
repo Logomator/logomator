06c948fa472deea300e94e2326bb0af7a889a523
Hunter Gemmer <hunterg325@gmail.com>
2017-04-18 21:58:22 -0400

    added new logo, start over icon, and new navbar styles.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-import logo from '../assets/images/logomator-logo.png';
+import logo from '../../../assets/images/logomator-logo.png';
 
 class Navbar extends Component {
 
     render() {
         return (
             <nav className="logomator-navbar">
-                <div className="container">
-                    <img src={logo} className="logo" alt="Logomator"/>
-                    <div className="right">
-                        <button className="login">Log in</button>
-                    </div>
+                <img src={logo} className="logo" alt="Logomator"/>
+                <div className="right">
+                    <button className="start-over">Start Over</button>
                 </div>
             </nav>
         );
