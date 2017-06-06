00fd0c075248f4889c5ca2591d5fffea6a57a97f
Hunter Gemmer <hunterg325@gmail.com>
2017-06-05 20:52:12 -0400

    push to heroku.

--- src/logo/actions.js
+++ src/logo/actions.js
@@ -76,7 +76,7 @@ export function fetchLogos (chars) {
   return dispatch => {
     dispatch(requestLogos(chars));
 
-    const URL = config.URLS.local + '/api/logos/chars';
+    const URL = config.URLS.heroku + '/api/logos/chars';
 
     const generateLogoRequest = {
       url: URL,
@@ -104,7 +104,7 @@ export function fetchLogos (chars) {
 export function fetchMoreLogos (chars) {
   return dispatch => {
     dispatch(requestMoreLogos(chars));
-    const URL = config.URLS.local + '/api/logos/concepts';
+    const URL = config.URLS.heroku + '/api/logos/concepts';
 
     const generateLogoRequest = {
       url: URL,
