4fc9f226860ae4907621a698275e5fc6a0afe61f
Hunter Gemmer <hunterg325@gmail.com>
2017-07-02 21:47:02 -0400

    fixed loading component showing when generating more logos.

--- src/components/common/LoadingComponent.js
+++ src/components/common/LoadingComponent.js
@@ -2,9 +2,9 @@ import React from 'react';
 import loadingGif from '../../assets/images/loading.gif';
 import ChatComponent from '../../components/common/ChatComponent';
 
-const LoadingComponent = ({ isFetching }) => {
+const LoadingComponent = ({ isFetching, isGeneratingMoreLogos }) => {
   return (
-      <div className={isFetching ? "loading-container show" : "loading-container"}>
+      <div className={isFetching && !isGeneratingMoreLogos ? "loading-container show" : "loading-container"}>
           <div className="loading">
             <ChatComponent text={<h1>Hang on a second, I’m designing your logo concepts.</h1>} />
             <img src={loadingGif} className="loading-gif" alt="Please wait while I generate your logos."/>
